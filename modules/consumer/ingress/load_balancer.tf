1a4203ff124baea6c026065a14ea1218ee319968
incho0824 <160176412+incho0824@users.noreply.github.com>
2025-08-25 10:52:59 -0400

    Remove legacy module directories

---
+++ modules/consumer/ingress/load_balancer.tf
@@ -0,0 +1,77 @@
+resource "google_compute_region_network_endpoint_group" "run_neg" {
+  project               = var.project_id
+  name                  = "${var.cloud_run_service_name}-neg"
+  network_endpoint_type = "SERVERLESS"
+  region                = var.region
+  cloud_run {
+    service = google_cloud_run_service.service.name
+  }
+}
+
+resource "google_compute_backend_service" "run_backend" {
+  project               = var.project_id
+  name                  = "${var.cloud_run_service_name}-backend"
+  load_balancing_scheme = "EXTERNAL"
+
+  backend {
+    group = google_compute_region_network_endpoint_group.run_neg.id
+  }
+}
+
+resource "google_compute_backend_bucket" "bucket_backend" {
+  project     = var.project_id
+  name        = "${var.bucket_name}-backend"
+  bucket_name = google_storage_bucket.static.name
+  enable_cdn  = true
+}
+
+resource "google_compute_url_map" "default" {
+  project         = var.project_id
+  name            = "${var.project_id}-url-map"
+  default_service = google_compute_backend_bucket.bucket_backend.id
+
+  host_rule {
+    hosts        = [var.domain]
+    path_matcher = "allpaths"
+  }
+
+  path_matcher {
+    name            = "allpaths"
+    default_service = google_compute_backend_bucket.bucket_backend.id
+
+    path_rule {
+      paths   = [var.api_path]
+      service = google_compute_backend_service.run_backend.id
+    }
+  }
+}
+
+resource "google_compute_managed_ssl_certificate" "default" {
+  project = var.project_id
+  name    = "${var.project_id}-cert"
+
+  managed {
+    domains = [var.domain]
+  }
+}
+
+resource "google_compute_target_https_proxy" "default" {
+  project          = var.project_id
+  name             = "${var.project_id}-https-proxy"
+  url_map          = google_compute_url_map.default.id
+  ssl_certificates = [google_compute_managed_ssl_certificate.default.id]
+}
+
+resource "google_compute_global_address" "lb_ip" {
+  project = var.project_id
+  name    = "${var.project_id}-lb-ip"
+}
+
+resource "google_compute_global_forwarding_rule" "https" {
+  project               = var.project_id
+  name                  = "${var.project_id}-https-fr"
+  target                = google_compute_target_https_proxy.default.id
+  port_range            = "443"
+  load_balancing_scheme = "EXTERNAL"
+  ip_address            = google_compute_global_address.lb_ip.address
+}
---
+++ modules/consumer/ingress/variables.tf
@@ -0,0 +1,36 @@
+variable "project_id" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "bucket_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "cloud_run_service_name" {
+  type = string
+}
+
+variable "cloud_run_image" {
+  type = string
+}
+
+variable "domain" {
+  type = string
+}
+
+variable "api_path" {
+  type    = string
+  default = "/api/*"
+}
+
+variable "member" {
+  type = string
+}
