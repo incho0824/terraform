a812719917e7f9e37e403c2777c665ae04b23b15
incho0824 <160176412+incho0824@users.noreply.github.com>
2026-02-11 20:15:03 -0500

    Delete modules/consumer-group directory

--- modules/consumer-group/ingress/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "bucket_name" {
-  value = google_storage_bucket.static.name
-}
-
-output "load_balancer_ip" {
-  value = google_compute_global_address.lb_ip.address
-}
--- modules/consumer-group/service/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "cloud_run_url" {
-  value = google_cloud_run_service.service.status[0].url
-}
-
-output "cloud_run_service_name" {
-  value = google_cloud_run_service.service.name
-}
