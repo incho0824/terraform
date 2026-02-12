8980ed41a510cc6d98da92050d6908a969b2ca01
incho0824 <160176412+incho0824@users.noreply.github.com>
2026-02-11 20:15:29 -0500

    Delete modules/tofu directory

--- modules/tofu/firestore.tf
+++
@@ -1,6 +0,0 @@
-# Enable Firestore API
-resource "null_resource" "firestore_database_default" {
-  triggers = {
-    firestore_database_name = data.terraform_remote_state.shared.outputs.firestore_database_name
-  }
-}
